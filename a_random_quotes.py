c665c12702e7fbdd8932839d54c177ba29ad0da1
hp <amd858@gmail.com>
2020-07-14 01:44:00 +0500

    some message
    2nd

---
+++ a_random_quotes.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class A1stRandomQuotesSpider(scrapy.Spider):
+    name = 'random_quote'
+    allowed_domains = ['toscrape.com']
+    start_urls = ['http://quotes.toscrape.com']
+
+    def parse(self, response):
+        self.log('i just hit with' + response.url)
+        for quote in response.css('div.quote'):
+            item = {
+                'author_name':quote.css('small.author::text').extract_first(),
+                'text':quote.css('span.text::text').extract_first(),
+                'tags':quote.css('a.tag::text').extract(),       
+            }
+            yield item
+            
+        next_page_url = response.css('li.next > a::attr(href)').extract_first()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
